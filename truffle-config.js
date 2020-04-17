4337c2ccfd73778be67d1f30f60371568405284f
VANRYSSEL Alexandre <avanryssel@gmail.com>
2020-04-17 14:35:13 +0200

    chore: add ganache network to truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -2,8 +2,13 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
+      port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+	ganache: {
+	  host: "127.0.0.1",
+	  port: 7545,
+	  network_id: "5777"
+	}
   }
 };
