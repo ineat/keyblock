93b324d0bd9e712a46d30676ed6f7a4d5c24ee23
VANRYSSEL Alexandre <avanryssel@gmail.com>
2020-04-17 16:58:07 +0200

    fix: make contracts compliant with solc 0.6.0

---
+++ truffle/contracts/ClaimHolder.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.6.0;
+
+import "./ERC735.sol";
+
+abstract contract ClaimHolder is ERC735 {
+    
+}
---
+++ truffle/contracts/KeyManager.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.6.0;
+
+import "./ERC734.sol";
+
+abstract contract KeyManager is ERC734 {
+
+}
