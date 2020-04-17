87daec582dd98794a35ae42769b2aa63615cec64
VANRYSSEL Alexandre <avanryssel@gmail.com>
2020-04-17 16:04:37 +0200

    chore: init project files

--- src/api.js
+++ src/api.js
@@ -7,22 +7,38 @@ var blockchain = require('./blockchain.js');
 var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 
+/**
+* Create a new identity for a new user
+*
+*/
 app.post("/identity", function(req, res){
 
 });
 
+/**
+* Create a new credential for a user
+*/
 app.post("/credential", function(req, res) {
 
 })
 
+/**
+* Update a credential
+*/
 app.put("/credential", function(req, res) {
 
 })
 
+/**
+* Revoke a credential
+*/
 app.get("/revoke", function(req, res) {
 
 })
 
+/**
+* Check access for user to a given service
+*/
 app.get("/verify", function(req, res) {
 
 })
--- src/blockchain.js
+++ src/blockchain.js
@@ -4,3 +4,36 @@ var fs = require("fs");
 var config = require("./config.js");
 
 var exports = module.exports = {};
+
+function connection() {
+
+}
+
+function initContract() {
+
+}
+
+exports.init = function() {
+	connection();
+	initContract();
+}
+
+exports.createIdentity = function() {
+
+}
+
+exports.createCredential = function() {
+
+}
+
+exports.updateCredential = function() {
+
+}
+
+exports.revokeCredential = function() {
+
+}
+
+exports.verify = function() {
+
+}
