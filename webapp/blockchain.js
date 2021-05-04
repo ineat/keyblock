fb5340264bffda626075dfee33db383b8940d7fd
avysel <avanryssel@gmail.com>
2021-05-04 14:27:42 +0200

    feat: improve signature display & check

--- webapp/blockchain.js
+++ webapp/blockchain.js
@@ -68,6 +68,10 @@ function authentication() {
     });
 }
 
+function checkSignature(signature, account) {
+
+}
+
 function checkClaims() {
     web3.eth.getAccounts()
     .then( async (accounts) => {
@@ -104,81 +108,66 @@ async function loadContract() {
     }
 }
 
+function messageHash(msg) {
+	return web3.utils.sha3('\x19Ethereum Signed Message:\n' + msg.length + msg);
+}
+
 async function askForSignature(account) {
 
- /*   var data = {
-        types:{
-            "EIP712Domain":[
-                {name:"name",type:"string"}
-                ,{name:"version",type:"string"}
-                ,{name:"chainId",type:"uint256"}
-                ,{name:"verifyingContract",type:"address"}
-            ]
-            ,Person:[
-                {name:"name",type:"string"}
-                ,{name:"wallet",type:"address"}
-            ]
-            ,Mail:[
-                {name:"from",type:"Person"}
-                ,{name:"to",type:"Person"}
-                ,{name:"contents",type:"string"}
-            ]
-           }
-        ,primaryType:"Mail"
-        ,domain:{name:"Keyblock",version:"1",chainId:3,verifyingContract:"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC"}
-        ,message:{
-            from:{name:"Cow",wallet:"0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826"}
-            ,to:{name:"Bob",wallet:"0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB"}
-            ,contents:"Hello, Bob!"
-        }
-    };
-*/
+    var data = {
+            types:{
+                "EIP712Domain":[
+                    {name:"name",type:"string"}
+                    ,{name:"version",type:"string"}
+                    ,{name:"chainId",type:"uint256"}
+                    ,{name:"verifyingContract",type:"address"}
+                ]
+                ,Contract:[
+                    {name:"name", type:"string"}
+                    ,{name:"address", type:"address"}
+                ]
+                ,User:[
+                    {name:"account", type:"address"}
+                ]
+                ,MessageToSign:[
+                      {name:"content",type:"string"}
+                      ,{name:"contract", type:"Contract"}
+                      ,{name:"forUser", type:"User"}
+                ]
 
-var data = {
-        types:{
-            "EIP712Domain":[
-                {name:"name",type:"string"}
-                ,{name:"version",type:"string"}
-                ,{name:"chainId",type:"uint256"}
-                ,{name:"verifyingContract",type:"address"}
-            ]
-            ,Contract:[
-                {name:"name", type:"string"}
-                ,{name:"address", type:"address"}
-            ]
-            ,User:[
-                {name:"account", type:"address"}
-            ]
-            ,MessageToSign:[
-                  {name:"content",type:"string"}
-                  ,{name:"contract", type:"Contract"}
-                  ,{name:"forUser", type:"User"}
-            ]
+               }
+            ,primaryType:"MessageToSign"
+            ,domain:{name:"Keyblock",version:"1",chainId:3,verifyingContract:"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC"}
+            ,message:{
+                content:"You are going to use your private key to sign this data and be authenticated for claim holder contract."
+                ,contract:{name:"Claim holder", address: contractAddress}
+                ,forUser:{account: account}
+                }
+        };
 
-           }
-        ,primaryType:"MessageToSign"
-        ,domain:{name:"Keyblock",version:"1",chainId:3,verifyingContract:"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC"}
-        ,message:{
-            content:"You are going to use your private key to sign this data and be authenticated for claim holder contract."
-            ,contract:{name:"Claim holder", address: contractAddress}
-            ,forUser:{account: account}
-            }
-    };
+    ethereum.request({
+        method: 'eth_signTypedData_v4',
+        params: [
+            account,
+            JSON.stringify(data)
+        ]
+    })
+    .then((signature) => {
 
-        ethereum
-          .request({
-            method: 'eth_signTypedData_v4',
-            params: [
-                account,
-                JSON.stringify(data)
-            ]
+        console.log("Signature: "+signature);
+        $('#userAuthent').text("Yes");
+        $('#signature').text(signature);
+
+        web3.eth.personal.ecRecover(JSON.stringify(data), signature)
+        .then(
+            (resultAccount) => {
+                $('#signatureCheck').text( (resultAccount==account) );
           })
-          .then((result) => {
-            console.log("Sign: "+result);
-            $('#userAuthent').text("Yes, with signature: "+result);
-          })
-          .catch((error) => {
-            console.log("Sign fail: "+error.message);
-            $('#userAuthent').text(error.message);
-          });
+        .catch(console.error);
+
+    })
+    .catch((error) => {
+        console.log("Sign fail: "+error.message);
+        $('#userAuthent').text(error.message);
+    });
 }
