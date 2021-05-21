85fc5aae5ebf797ae8bfdbc58330cd0e3b044405
avysel <avanryssel@gmail.com>
2021-05-21 12:31:56 +0200

    feat(contracts): fix contract compilation script

--- contracts/compile.sh
+++ contracts/compile.sh
@@ -8,11 +8,17 @@
 # sudo apt-get install solc
 
 # compile contract
+echo "Compile..."
 solc ClaimsRegistry.sol --bin --abi -o ./build/ --overwrite
 
-# cpy abi and binary to java connector
+# copy abi and binary to java connector
+echo "Copy file to java connector..."
 cp ./build/ClaimsRegistry.abi ../java/blockchain-connector/src/main/resources/ClaimsRegistry.abi
 cp ./build/ClaimsRegistry.bin ../java/blockchain-connector/src/main/resources/ClaimsRegistry.bin
 
 # generate web3j classe for contract
+echo "Generate java wrapper..."
+cd build
 web3j generate solidity -a=ClaimsRegistry.abi -b=ClaimsRegistry.bin -o=../java/blockchain-connector/src/main/java/com/keyblock/contract --package=com.keyblock.contract
+
+echo "Done"
